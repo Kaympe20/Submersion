d89a53ac22a968fa2bde72222105d126cf7d2a03
Kayla Blosser <kayla.blosser@proton.me>
2025-02-14 14:28:16 -0500

    Merge @ebor62's fire codepen changes

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const sections = document.querySelectorAll('.wave-section');
     let lastScrollPosition = window.scrollY;
-    
+
     window.addEventListener('scroll', () => {
         const scrollPosition = window.scrollY;
         const windowHeight = window.innerHeight;
@@ -12,4 +12,17 @@ document.addEventListener('DOMContentLoaded', () => {
         section.style.transform = `translateY(${translateY}%)`;
         });
     });
+
+    const wavingWord = (word, timingDelay = 0.1) => {
+        const wavingWord = document.getElementById('wavingWord');
+        for (let i = 0; i < word.length; i++) {
+        const wordEl = document.createElement('div');
+        wordEl.classList.add('word');
+        wordEl.innerHTML = word[i];
+        wordEl.style.animationDelay = `${(i * timingDelay).toFixed(1)}s`;
+        wavingWord.append(wordEl);
+        }
+    };
+
+    wavingWord('SUBMERSION');
 });
